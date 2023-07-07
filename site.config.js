f3f9ddbc33642786db8ced9f89dfaad833f2c556
Junhyun <junhyun@innerviewit.com>
2023-07-07 14:12:44 +0900

    [fix] 구글애널리틱스, searchconsole.

--- site.config.js
+++ site.config.js
@@ -40,15 +40,15 @@ const CONFIG = {
 
   // plugin configuration (optional)
   googleAnalytics: {
-    enable: false,
+    enable: true,
     config: {
-      measurementId: process.env.NEXT_PUBLIC_GOOGLE_MEASUREMENT_ID || "",
+      measurementId: process.env.NEXT_PUBLIC_GOOGLE_MEASUREMENT_ID,
     },
   },
   googleSearchConsole: {
-    enable: false,
+    enable: true,
     config: {
-      siteVerification: process.env.NEXT_PUBLIC_GOOGLE_SITE_VERIFICATION || "",
+      siteVerification: process.env.NEXT_PUBLIC_GOOGLE_SITE_VERIFICATION,
     },
   },
   utterances: {
